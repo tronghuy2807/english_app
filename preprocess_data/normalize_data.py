20113ec973a114b6e368a07c375369f1a07fecdc
tronghuy2807 <tronghuy2807@gmail.com>
2018-10-16 22:47:49 +0700

    corrected dictianary format

--- preprocess_data/normalize_data.py
+++ preprocess_data/normalize_data.py
@@ -4,7 +4,6 @@ def find_wrong_line():
     with open('../data/dictionary.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter='|')
         line_count = 0
-
         for row in csv_reader:
             try:
                 line_count += 1
