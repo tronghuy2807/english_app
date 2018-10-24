6b9f1ca3c469971cd5e6ebe886bbf0313b63d4a8
Huy Do <huydt@nextsmarty.com>
2018-10-24 14:52:21 +0700

    strim for each column in dict

--- englishcommonwords.py
+++ englishcommonwords.py
@@ -1,11 +1,19 @@
 from processdata import CSVData
+from utils.normalize import NormalizeWord
+
 
 def main():
-    #path to data"
+    # path to data"
     path_data = "data/dictionary.csv"
-    #Load data from csv
+    # Load data from csv
     csv = CSVData()
     english_words = csv.load_data(path_data)
-    print 'xxx'
+    # strip() for all column in dict
+    nm = NormalizeWord()
+    english_words = nm.trim_all_column(english_words)
+
+    print english_words.head(3)
+
+
 if __name__ == "__main__":
-    print main()
+    main()
--- utils/normalize.py
+++ utils/normalize.py
@@ -13,6 +13,15 @@ class Manual:
                     print line_count
                     print "-----------"
 
+class NormalizeWord:
+    def trim_all_column(self,df):
+        """
+        Trim whitespace from ends of each value across all series in dataframe
+        """
+        trim_string = lambda x: x.strip() if type(x) is str else x
+        return df.applymap(trim_string)
+
+
 if __name__ == "__main__":
     manual = Manual()
     manual.find_wrong_line()
